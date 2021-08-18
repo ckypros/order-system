151551311c6decd935596367b2e72063fdd31340
ckypros <ckypros@gmail.com>
2021-08-17 22:00:15 -0700

    added basic meal UI

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,5 @@
+import MealItemForm from './MealItemForm';
+
 import classes from './MealItem.module.css';
 
 const MealItem = ({ meal }) => {
@@ -10,7 +12,9 @@ const MealItem = ({ meal }) => {
         <div className={classes.description}>{meal.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
