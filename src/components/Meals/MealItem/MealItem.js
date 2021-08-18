d70f8d9e47091a49346a11261b78d7d4de1380f1
ckypros <ckypros@gmail.com>
2021-08-17 20:18:07 -0700

    first commit

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,18 @@
+import classes from './MealItem.module.css';
+
+const MealItem = ({ meal }) => {
+  const price = `$${meal.price.toFixed(2)}`;
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{meal.name}</h3>
+        <div className={classes.description}>{meal.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
+
+export default MealItem;
